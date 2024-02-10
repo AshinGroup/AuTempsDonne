8bfc2f28ae1fa7d23f81c5c141fccab2e3d81355
Frederic Huang <huangfrederic2002@gmail.com>
2024-02-10 20:54:59 +0100

    Setup API Handling using file_get_contents and curl

---
+++ web/backend/components/navbar.php
@@ -0,0 +1,32 @@
+<!-- Navbar -->
+<nav class="navbar navbar-expand-lg sticky-top bg-body-tertiary">
+    <div class="container-fluid">
+        <a class="ps-2" href="/"><img src="../ressources/logo.png" alt="Logo" width="50"></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="/">Accueil</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/">Administration</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Langue
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        <a class="dropdown-item" href="#">Français</a>
+                        <a class="dropdown-item" href="#">Anglais</a>
+                        <a class="dropdown-item" href="#">Chinois</a>
+                    </div>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/">Deconnexion</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
