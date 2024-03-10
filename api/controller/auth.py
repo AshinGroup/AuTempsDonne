e8e11248b8e5ab746445526eb01e754198030b4a
Franck Zhuang <franck.zhuang@hotmail.com>
2024-03-10 15:45:33 +0100

    Start jwt token auth

---
+++ api/controller/auth.py
@@ -0,0 +1,9 @@
+from flask_restful import Resource
+from flask import jsonify
+
+class LoginController(Resource):
+    def __init__(self) -> None:
+        pass  
+
+    def post(self):
+        pass
