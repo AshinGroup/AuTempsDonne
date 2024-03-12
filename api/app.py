daf12921e347a5e15ce47430b0c82efdf49159f0
Franck Zhuang <franck.zhuang@hotmail.com>
2024-03-12 12:05:08 +0100

    Delete api

--- api/app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from flask_cors import CORS
-import os
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+mysqlconnector://{os.getenv('HOST_USER')}:{os.getenv('HOST_PASSWORD')}@{os.getenv('HOSTNAME')}/{os.getenv('DB_NAME')}"
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.sort_keys = False
-CORS(app)
--- api/function/hash.py
+++
@@ -1,7 +0,0 @@
-from passlib.hash import sha256_crypt
-
-def hash_password(password: str) -> sha256_crypt:
-    return sha256_crypt.using(rounds=5000).hash(password)
-
-def verify_hash(password: str, hashed: sha256_crypt) -> bool:
-    return sha256_crypt.verify(password, hashed)
