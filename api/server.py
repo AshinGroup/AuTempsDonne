dba553d40be50b3b5c2359c2f76e338234d78a61
arkayzdev <franck.zhuang@gmail.com>
2024-04-18 01:15:47 +0200

    Fix docker-compose

--- api/server.py
+++ api/server.py
@@ -124,4 +124,4 @@ if __name__ == "__main__":
     with app.app_context():
         # db.drop_all()
         db.create_all()
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=6001, debug=True)
