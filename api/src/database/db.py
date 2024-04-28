d68bec2db7c1f8b25d6fc5a60e624e60b1ba1253
arkayzdev <franck.zhuang@gmail.com>
2024-04-28 17:06:30 +0200

    Fix docker

--- api/src/database/db.py
+++ api/src/database/db.py
@@ -4,18 +4,6 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-def create_database():
-    mydb = mysql.connector.connect(
-      host = os.getenv("HOSTNAME"),
-      user = os.getenv("HOST_USER"), 
-      password = os.getenv("HOST_PASSWORD")
-    )
-
-    mycursor = mydb.cursor()
-
-    mycursor.execute(f"CREATE DATABASE IF NOT EXISTS {os.getenv('DB_NAME')}")
-    mydb.close()
-
 
 
  
