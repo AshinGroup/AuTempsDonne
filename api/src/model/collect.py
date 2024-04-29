d4e038eeec94498e69f0027a94326df7f5ab2335
arkayzdev <franck.zhuang@gmail.com>
2024-04-30 01:37:23 +0200

    Start setup colect

---
+++ api/src/model/collect.py
@@ -0,0 +1,35 @@
+from database.db import db
+import os
+
+
+class Collect(db.Model):
+    __tablename__ = "collect"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    submitted_datetime = db.Column(db.DateTime)
+    limit_datetime = db.Column(db.DateTime)
+    status = db.Column(db.Integer) # 0 = Submitted / 1 = In Progress / 2 = Collected
+    additional = db.Column(db.Text)
+    shop_id = db.Column(db.Integer, db.ForeignKey('shop.id'), nullable=False)
+ 
+
+    def json(self):
+        return {'id': self.id,
+                'submitted_datetime': self.submitted_datetime.strftime("%Y-%m-%d %H:%M:%S"),
+                'limit_datetime': self.limit_datetime.strftime("%Y-%m-%d %H:%M:%S"),
+                'status': self.status,
+                'additional': self.additional,
+                'shop' : self.shop.json_shop()
+                }
+
+
+    def json_rest_shop(self):
+        return {'url': f"{os.getenv('API_PATH')}/collect/{self.id}",
+                'id': self.id,
+                'submitted_datetime': self.submitted_datetime.strftime("%Y-%m-%d %H:%M:%S"),
+                'limit_datetime': self.limit_datetime.strftime("%Y-%m-%d %H:%M:%S"),
+                'status': self.status,
+                'additional': self.additional
+                }
+
+
