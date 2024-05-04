eeb4f848f0b3a2393d569eab03130361d5085160
arkayzdev <franck.zhuang@gmail.com>
2024-05-04 14:05:16 +0200

    Fix init.Sql

--- sql/init.sql
+++ sql/init.sql
@@ -11,9 +11,9 @@ USE atd;
 -- -- Version du serveur : 8.4.0
 -- -- Version de PHP : 8.2.18
 
--- SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
--- START TRANSACTION;
--- SET time_zone = "+00:00";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
 -- /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
